17ada090a1e94b143e52ec31372f525f14d40d85
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2025-03-25 22:34:27 +0530

    auth routes implement with express validation.

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const http = require('http');
+const socketIo = require('socket.io');
 const sequelize = require("./src/config/database.js");
-const User = require('./src/models/User.js');
-const Post = require('./src/models/Post.js');
+const authController = require('./src/controllers/authController.js');
+const { validationResult } = require('express-validator');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 app.use(express.json());
 dotenv.config();
 
 app.use((req, res, next) => {
+    req.io = io;
     next();
 });
+
+const validate = (validations) => {
+    return async (req, res, next) => {
+        await Promise.all(validations.map((validation) => validation.run(req)));
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+        next();
+    };
+};
+
+// Auth Routes
+app.post('/auth/register', validate(authController.validationRules.register), authController.register);
+app.post('/auth/login', validate(authController.validationRules.login), authController.login);
 
 sequelize.sync().then(() => {
     app.listen(process.env.PORT, () => {
