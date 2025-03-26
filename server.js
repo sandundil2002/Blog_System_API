d51993e32a5c52e2cdda690ee74e4fc9f59952f0
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2025-03-26 12:48:40 +0530

    blog post routes implement in server.

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const http = require('http');
 const socketIo = require('socket.io');
 const sequelize = require("./src/config/database.js");
 const authController = require('./src/controllers/authController.js');
+const postController = require('./src/controllers/postController.js');
+const authMiddleware = require('./src/middleware/authMiddleware.js');
+const errorHandler = require('./src/middleware/errorHandler.js');
+const cookieParser = require('cookie-parser');
 const { validationResult } = require('express-validator');
 
 const app = express();
+app.use(cookieParser());
 const server = http.createServer(app);
 const io = socketIo(server);
 app.use(express.json());
@@ -26,9 +31,16 @@ const validate = (validations) => {
     };
 };
 
-// Auth Routes
 app.post('/auth/register', validate(authController.validationRules.register), authController.register);
 app.post('/auth/login', validate(authController.validationRules.login), authController.login);
+
+app.get('/posts', postController.getAllPosts);
+app.get('/posts/:id', postController.getPostById);
+app.post('/posts', authMiddleware, postController.createPost);
+app.put('/posts/:id', authMiddleware, postController.updatePost);
+app.delete('/posts/:id', authMiddleware, postController.deletePost);
+
+app.use(errorHandler);
 
 sequelize.sync().then(() => {
     app.listen(process.env.PORT, () => {
